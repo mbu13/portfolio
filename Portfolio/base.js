f4a75b04872e3d38dbe64d52dd1bc4b57e4d23bf
mbu13 <matthewbu13@gmail.com>
2018-12-31 23:21:56 -0500

    Add files via upload

--- Portfolio/base.js
+++ Portfolio/base.js
@@ -1,9 +1,21 @@
   $(document).ready(function(){
-    $('.parallax').parallax();
+  	$('.parallax').parallax();
 
-    $('#lang-2').hide();
+  	$('#lang-2').hide();
 
-    setInterval(
+  	$('#copyright-year').text((new Date()).getFullYear());
+
+  	var introViewed = false;
+  	var timelineViewed = false;
+  	var skillsViewed = false;
+
+    // Set age
+    var dob = new Date("1996-04-01");
+    var today = new Date();
+    var age = Math.floor((today-dob) / (365.25 * 24 * 60 * 60 * 1000));
+    $('#age').text(age);
+
+    /*setInterval(
 		function() {   
 		    if ($('#lang-2').is(":hidden")) {
 		    	$('#lang-1').hide();
@@ -13,12 +25,60 @@
 		    	$('#lang-1').fadeIn();
 		    }
 		},
-		5000  /* 10000 ms = 10 sec */
-	);
+		5000
+		);*/
 
-    $("#down-button").click(function() {
-    	$('html,body').animate({
-        	scrollTop: $(".first-element").offset().top},
-        	'slow');
+		$("#down-button").click(function() {
+			$('html,body').animate({
+				scrollTop: $("#first-element").offset().top - 100},
+				'slow');
 		});
-  });
+
+		$(".intro").each(function() {
+
+			$(this).waypoint(function() {
+				if (!introViewed) {
+					introViewed = true;
+					var cols = $(".col");
+					cols.each(function(indx){
+						$(this).delay(indx * 200).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0});
+					});
+				}
+			}, {
+				triggerOnce: true,
+				offset: '50%'
+			});
+		});
+
+		$(".timeline").each(function() {
+
+			$(this).waypoint(function() {
+				if (!timelineViewed) {
+					timelineViewed = true;
+					var companies = $(".circle-img-left");
+					companies.each(function(indx){
+						$(this).delay(indx * 200).fadeIn(750);
+					});
+				}
+			}, {
+				triggerOnce: true,
+				offset: '50%'
+			});
+		});
+
+		$(".skills").each(function() {
+
+			$(this).waypoint(function() {
+				if (!skillsViewed) {
+					skillsViewed = true;
+					var progressBar = $(".determinate");
+					progressBar.each(function(indx){
+						$(this).animate({"width": $(this).attr("value") + "%"}, 750);
+					});
+				}
+			}, {
+				triggerOnce: true,
+				offset: '50%'
+			});
+		});
+	});
